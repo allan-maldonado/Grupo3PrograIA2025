85704b661445e807b1d8709a9b74fe704cec9f63
danie <danie@DANIEL>
2025-05-09 23:24:32 -0600

    menu para registrar animales comida y a que clase pertenecen y guardar los animales en CSV

--- Grupo1PrograIA2025/ZooAurora/src/main/java/com/mycompany/zooaurora/Mamifero.java
+++ Grupo1PrograIA2025/ZooAurora/src/main/java/com/mycompany/zooaurora/Mamifero.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.zooaurora;
 
-/**
- *
- * @author Tecno
- */
 class Mamifero extends Animal {
-    public Mamifero(int IdAnimal, String nombre, double consumoDiario) {
-        super(IdAnimal, nombre, "Mamífero", consumoDiario);
+    public Mamifero(String idAnimal, String nombre, double consumoDiario) {
+        super(idAnimal, nombre, "Mamífero", consumoDiario);
     }
 
     @Override
     public void mostrarInfo() {
-        System.out.println("El ID es "+IdAnimal+"Mamífero: " + nombre + " | Consumo diario: " + consumoDiario + " libras");
+        System.out.println("El ID es " + getIdAnimal() + " | Tipo: Mamífero | Nombre: " + getNombre() + " | Consumo diario: " + getConsumoDiario() + " libras");
+    }
+
+    @Override
+    public String getIdAnimal() {
+        return super.getIdAnimal();
+    }
+
+    @Override
+    public String getNombre() {
+        return super.getNombre();
+    }
+
+    @Override
+    public double getConsumoDiario() {
+        return super.getConsumoDiario();
     }
 }
