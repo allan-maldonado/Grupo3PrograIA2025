0b8816441fe190bc96545072a812f61496ce6531
PGutierrez00 <pgutierrezp2@miumg.edu.gt>
2026-03-22 16:27:41 -0600

    initial commit

--- Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Progra1BGrupo12026.java
+++ Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Progra1BGrupo12026.java
@@ -7,9 +7,64 @@ public class Progra1BGrupo12026 {
     public static void main(String[] args) {
         
         
-        System.out.println("Que onda grupo ya arrancamos je je je");
+        System.out.println("****************************************");
+        System.out.println("***    Bienvenido a Zoo La Aurora   ****");
+        System.out.println("**************************************** \n");
+
+        Scanner in = new Scanner(System.in);
+        String datos; String nombre; int edad; int repetir, opcion = 0;
+        
+        System.out.println("Sea bievenido al Zoo la Aurora \n"
+                + "por favor seleccione una opcion del menu: \n");
+       
+        do{
+        
+        System.out.println("Menu: \n" + 
+                "1. Nuevo Registro \n" +
+                "2. Lista de animales \n" + 
+                "3. Horario Alimentacion \n" + 
+                "4. Salir \n");
+        opcion = in.nextInt();
+        
+        switch (opcion){
+        
+            case 1: System.out.println("Usted acaba de seleccionar Nuevo Ingreso: ");
+            
+            
+            break;
+            
+            case 2: System.out.println("Usted seleeciono Lista de Animales: ");
+            
+            
+            break;
+            
+            case 3: System.out.println("Usted selecciono Horario de Alimentacion: ");
+            
+            
+            break;
+            
+            case 4: System.out.println("Usted selecciono Salir: ");
+            
+            
+            break;
+            
+            default:
+                System.out.println("Opcion invalida, por favor ingrese nuevamente: ");
+                
+                break;
+        }
         
         
-        System.out.println("Hello World!");
+            System.out.println("Si desea continuar presione 1 0 2 para salir.");
+            repetir = in.nextInt();
+        
+            
+        } while (repetir == 1);
+        System.out.println("Gracias por su atencion y cuidado de los animales \n"
+                + "del Zoologico la Aurora de la Ciudad de Gautemala.");
+    
+    
     }
 }
+
+    
