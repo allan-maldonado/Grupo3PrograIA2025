1b88d4b0462225c499d1c612215cc8c4a93793c9
PGutierrez00 <pgutierrezp2@miumg.edu.gt>
2026-03-22 02:35:14 -0600

    Update Progra1BGrupo12026.java

--- Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Progra1BGrupo12026.java
+++ Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Progra1BGrupo12026.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package gt.edu.miumg.progra1bgrupo12026;
+
+import java.util.Scanner;
 
 public class Progra1BGrupo12026 {
 
     public static void main(String[] args) {
+        
+        
+        System.out.println("Que onda grupo ya arrancamos je je je");
+        
+        
         System.out.println("Hello World!");
     }
 }
