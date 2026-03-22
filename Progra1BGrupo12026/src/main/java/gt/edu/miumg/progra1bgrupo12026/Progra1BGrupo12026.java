ed7964cd88a04e76e9fff5cca4029ba935c4986b
PGutierrez00 <pgutierrezp2@miumg.edu.gt>
2026-03-22 01:47:06 -0600

    initial commit

---
+++ Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Progra1BGrupo12026.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gt.edu.miumg.progra1bgrupo12026;
+
+public class Progra1BGrupo12026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
