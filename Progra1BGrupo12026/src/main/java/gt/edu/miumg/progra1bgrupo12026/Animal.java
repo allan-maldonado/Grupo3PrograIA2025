2b8dacfa7151130055f3007c91643db60c534491
PGutierrez00 <pgutierrezp2@miumg.edu.gt>
2026-03-28 02:30:29 -0600

    agregando las primeras lineas de codigo

--- Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Animal.java
+++ Progra1BGrupo12026/src/main/java/gt/edu/miumg/progra1bgrupo12026/Animal.java
@@ -1,3 +1,5 @@
+package gt.edu.miumg.progra1bgrupo12026;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
